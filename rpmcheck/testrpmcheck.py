24d64f63c8c0f738708430f322fb715e76b2891d
Aliaksandr Patapniou <32715736+apatapniou@users.noreply.github.com>
2018-11-23 17:37:37 +0300

    Update testrpmcheck.py

--- rpmcheck/testrpmcheck.py
+++ rpmcheck/testrpmcheck.py
@@ -1,10 +1,29 @@
 import re
 import wget
-f = open('C:\\Users\Aliaksandr_Patapniou\Documents\\test.txt')
-rpmlits = []
-for line in f:
-    result = re.findall(r'href=\"(.*?)"\>', line)
-    r = ''.join(result)
-    url = 'https://rpmfind.net/linux/fedora/linux/updates/29/Everything/aarch64/Packages/k/%s' % (r)
-    rpmlits.append(url)
-print(rpmlits)
+file_new = open('C:\\Users\Aliaksandr_Patapniou\Documents\\test.txt')
+rpmname = []
+rpmname_new = []
+
+file_old = open('C:\\Users\Aliaksandr_Patapniou\Documents\\test1.txt', 'r+')
+site = 'http://mirror.datacenter.by/pub/fedoraproject.org/linux/releases/29/Everything/x86_64/os/Packages/k/%s'
+
+for line in file_old:
+    result = ''.join(re.findall(r'kernel-.*.rpm', line))
+    time = ''.join(re.findall(r'\d{2}-\w+-\d+.\w+:\d{2}', line))
+    rpmname.append(result + ' ' + time)
+
+for line in file_new:
+    result_new = ''.join(re.findall(r'href=\"(kernel-.*?)"\>', line))
+    time_new = ''.join(re.findall(r'href=\"kernel-.*?"\>.*(\d{2}-\w+-\d+.\w+:\d{2})', line))
+    rpmname_new.append(result_new + ' ' + time_new)
+
+diffrpm = list(set(rpmname_new).difference(set(rpmname)))
+
+for i in range(len(diffrpm)):
+    rpm = ''.join(re.findall(r'kernel-.*.rpm', diffrpm[i]))
+    url = site % rpm
+    wget.download(url, 'C:\\Users\Aliaksandr_Patapniou\Documents\\%s' % (rpm))
+    file_old.write(diffrpm[i] + "\n")
+
+file_old.close()
+file_new.close()
